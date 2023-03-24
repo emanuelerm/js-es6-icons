5cd3311c8981ccf3f618360642cdeb971d785aa8
emanuelerm <emanueler00@gmail.com>
2023-03-24 15:43:49 +0100

    creato una funzione per generare tutte le card

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ Milestone 2
 Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
 Milestone 3
-Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). 
+Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
 
 
 BONUS
 
-1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, 
+    sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 
 2- popolare le options della select della milestone 3 dinamicamente.
 
@@ -26,3 +28,146 @@ Come possiamo usare i dati presenti nella nostra struttura dati per creare l'ele
 Inizialmente può essere sufficiente stampare dei semplici div, senza alcuno stile, con all'interno l'icona e uno span con il nome. 
 Solamente quando la parte logica è completa, ci dedichiamo al css.
 */
+
+// array degli oggetti
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    }
+];
+
+// attraverso la funzione creo un array di carte
+function createCards(icon) {
+    // dichiaro l'array e lo lascio vuoto
+    let cardsTpl = [];
+    // per ogni oggetto dell'array icons creo una singola card
+    for (let i = 0; i < icons.length; i++) {
+        const icon = icons[i];
+        const cardTpl = `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                <div class="card text-bg-light mb-3">
+                    <div class="card-body text-center">
+                        <p class="card-icon"><i>${icon.prefix}${icon.family} ${icon.prefix}${icon.name}</i></p>
+                        <p class="card-text">${icon.name}</p>
+                    </div>
+                </div>
+            </div>`;
+        // terminata la carta, la inserisco all'interno dell'array vuoto
+        cardsTpl.push(cardTpl);
+    }
+    // eseguito tutto il ciclo, ritorno come risultato l'array delle card
+    return cardsTpl;
+    
+}
+
+// invoco la funzione che crea le card
+createCards(icons);
